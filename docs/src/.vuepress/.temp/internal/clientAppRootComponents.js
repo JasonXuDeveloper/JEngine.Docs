ae102f7fcc34178c32ee3fab60bf16ae61e982ba
JasonXuDeveloper - 傑 <jasonxudeveloper@gmail.com>
2025-04-06 19:54:15 +1000

    Delete docs/src/.vuepress/.temp directory

--- docs/src/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from '/www/wwwroot/GameDocs/docs/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]
--- docs/src/.vuepress/.temp/pages/zh/guide/using-vue.html.vue
+++
@@ -1,6 +0,0 @@
-<template><h1 id="使用" tabindex="-1"><a class="header-anchor" href="#使用" aria-hidden="true">#</a> 使用</h1>
-<h2 id="browser-api-access-restrictions" tabindex="-1"><a class="header-anchor" href="#browser-api-access-restrictions" aria-hidden="true">#</a> Browser API Access Restrictions</h2>
-<p>Because VuePress applications are server-rendered in Node.js when generating static builds, any Vue usage must conform to the <a href="https://ssr.vuejs.org/en/universal.html" target="_blank" rel="noopener noreferrer">universal code requirements<ExternalLinkIcon/></a>. In short, make sure to only access Browser / DOM APIs in <code>beforeMount</code> or <code>mounted</code> hooks.</p>
-<p>If you are using or demoing components that are not SSR friendly (for example containing custom directives), you can wrap them inside the built-in <code>&lt;ClientOnly&gt;</code> component:</p>
-<h2 id="" tabindex="-1"><a class="header-anchor" href="#" aria-hidden="true">#</a> </h2>
-</template>
